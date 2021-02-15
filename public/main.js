fcea38ab0599d36502bdd03cf355206f70a33c9d
Pierce Hunt <huntpie@gmail.com>
2021-02-15 12:54:27 -0500

    Not the prettiest but functional

--- public/main.js
+++ public/main.js
@@ -25,51 +25,55 @@ function updateTeam2NameAlt(){
 
 function updateTeam1ScorePlus(event){
   const newTeam1Score = document.querySelector('h3.team1-score')
-  console.log(newTeam1Score)
-  if (newTeam1Score.textContent < 21) {
-  newTeam1Score.textContent++
-  } 
+  const displayWinner1 = document.querySelector('h3.winnerDisplay1')
+  if (newTeam1Score.textContent < 20) {
+    newTeam1Score.textContent++
+    } else if (newTeam1Score.textContent = 21) {
+      displayWinner1.style.visibility = 'visible'
+  }
 }
 function updateTeam1ScoreSubtract(event){
   const newTeam1Score = document.querySelector('h3.team1-score')
+  const displayWinner1 = document.querySelector('h3.winnerDisplay1')
   console.log(newTeam1Score)
-  if (newTeam1Score.textContent > 0) {
+  if (newTeam1Score.textContent > 0 ) {
     newTeam1Score.textContent--
-  }
+    displayWinner1.style.visibility = 'hidden'
+  } 
 }
 function updateTeam2ScorePlus(event){
   const newTeam2Score = document.querySelector('h3.team2-score')
+  const displayWinner2 = document.querySelector('h3.winnerDisplay2')
   console.log(newTeam2Score)
-  if (newTeam2Score.textContent < 21) {
+  if (newTeam2Score.textContent < 20) {
   newTeam2Score.textContent++
-  }
+  } else if (newTeam2Score.textContent = 21) {
+    displayWinner2.style.visibility = 'visible'
+}
 }
 function updateTeam2ScoreSubtract(event){
   const newTeam2Score = document.querySelector('h3.team2-score')
+  const displayWinner2 = document.querySelector('h3.winnerDisplay2')
   console.log(newTeam2Score)
   if (newTeam2Score.textContent > 0) {
     newTeam2Score.textContent--
-  }
+    displayWinner2.style.visibility = 'hidden'
 }
-function winnerPopUp(event){
-  const team2Score = document.querySelector('h3.team2-score')
-  const team1Score = document.querySelector('h3.team1-score')
-  const displayWinner1 = document.querySelector('h3.winnerDisplay1')
-  const displayWinner2 = document.querySelector('h3.winnerDisplay2')
-    if (team1Score.textContent = 21) {
-    displayWinner1.removeAttribute('hidden')
-    } else if (team2Score.textContent = 21){
-      displayWinner2.removeAttribute('hidden')
-    }
 }
-function updateAllScores(event){
-  const zeroScore1 = document.querySelector('h3.team1-score')
+
+function updateAllScores(){
   const zeroScore2 = document.querySelector('h3.team2-score')
+  const zeroScore1 = document.querySelector('h3.team1-score')
+  const displayWinner2 = document.querySelector('h3.winnerDisplay2')
   const displayWinner1 = document.querySelector('h3.winnerDisplay1')
-  displayWinner1.addAttribute('hidden')
-  zeroScore1.textContent == 0
-  zeroScore2.textContent == 0
+  if (zeroScore2.textContent >= 0 && zeroScore1.textContent >= 0) {
+    zeroScore2.textContent = 0
+    zeroScore1.textContent = 0
+    displayWinner2.style.visibility = 'hidden'
+    displayWinner1.style.visibility = 'hidden'
+} 
 }
+
 
 
 
@@ -78,12 +82,11 @@ function main() {
   document.querySelector('input.update-team2-name').addEventListener('input', updateTeam2Name)
   document.querySelector('button.update-team1-name-button').addEventListener('click', updateTeam1NameAlt)
   document.querySelector('button.update-team2-name-button').addEventListener('click', updateTeam2NameAlt)
-  document.querySelector('button.team1-score-button-plus').addEventListener('click',updateTeam1ScorePlus)
-  document.querySelector('button.team1-score-button-subtract').addEventListener('click',updateTeam1ScoreSubtract)
-  document.querySelector('button.team2-score-button-plus').addEventListener('click',updateTeam2ScorePlus)
-  document.querySelector('button.team2-score-button-subtract').addEventListener('click',updateTeam2ScoreSubtract)
-  document.querySelector('button.reset-scores').addEventListener('click',updateAllScores)
-  winnerPopUp()
+  document.querySelector('button.team1-score-button-plus').addEventListener('click', updateTeam1ScorePlus)
+  document.querySelector('button.team1-score-button-subtract').addEventListener('click', updateTeam1ScoreSubtract)
+  document.querySelector('button.team2-score-button-plus').addEventListener('click', updateTeam2ScorePlus)
+  document.querySelector('button.team2-score-button-subtract').addEventListener('click', updateTeam2ScoreSubtract)
+  document.querySelector('button.reset-scores').addEventListener('click', updateAllScores)
 }
 
 
